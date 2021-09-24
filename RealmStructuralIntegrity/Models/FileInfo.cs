e1c37b3dde3ab6f6824fd8b097e46bc247c30388
Dean Herbert <pe@ppy.sh>
2021-09-24 13:32:45 +0900

    Switch `FileInfo`'s primary key to the hash
    There's no need to store a GUID in this case.

--- RealmStructuralIntegrity/Models/FileInfo.cs
+++ RealmStructuralIntegrity/Models/FileInfo.cs
@@ -1,21 +1,24 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.IO;
-using osu.Game.Database;
 using Realms;
 
 namespace osu.Game.Models
 {
-    public class FileInfo : RealmObject, IHasGuidPrimaryKey
+    public class FileInfo : RealmObject
     {
-        public Guid ID { get; set; }
-
+        /// <summary>
+        /// SHA-256 hash of the file content.
+        /// </summary>
+        [PrimaryKey]
         public string Hash { get; set; }
 
-        public string StoragePath => Path.Combine(Hash.Remove(1), Hash.Remove(2), Hash);
-
+        /// <summary>
+        /// The number of times this file is referenced across all usages.
+        /// </summary>
         public int ReferenceCount { get; set; }
+
+        public string StoragePath => Path.Combine(Hash.Remove(1), Hash.Remove(2), Hash);
     }
 }
