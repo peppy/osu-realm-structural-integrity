f6a18704b6c526e6ccda05b699b3a5c01b471525
Dean Herbert <pe@ppy.sh>
2021-09-24 13:47:08 +0900

    Use `EmbeddedObject` for more 1-1 relations

--- RealmStructuralIntegrity/Models/BeatmapSetFileInfo.cs
+++ RealmStructuralIntegrity/Models/BeatmapSetFileInfo.cs
@@ -1,16 +1,13 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using osu.Game.Database;
 using Realms;
 
 namespace osu.Game.Models
 {
-    public class BeatmapSetFileInfo : RealmObject, INamedFileInfo, IHasGuidPrimaryKey
+    public class BeatmapSetFileInfo : EmbeddedObject, INamedFileInfo
     {
-        public Guid ID { get; set; }
-
         public FileInfo FileInfo { get; set; }
 
         public string Filename { get; set; }
