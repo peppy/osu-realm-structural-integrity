47d8f1c38492d16d29c96c8177db0423e6cffdab
Dean Herbert <pe@ppy.sh>
2021-09-25 13:36:19 +0900

    Refactor `Live` to support scoped access only

--- RealmStructuralIntegrity/Database/Live.cs
+++ RealmStructuralIntegrity/Database/Live.cs
@@ -2,8 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Diagnostics;
-using osu.Framework.Development;
 using Realms;
 
 #nullable enable
@@ -14,37 +12,43 @@ namespace osu.Game.Database
     /// Provides a method of working with realm objects over longer application lifetimes.
     /// </summary>
     /// <typeparam name="T">The underlying object type.</typeparam>
-    public class Live<T> : IRealmBindableActions
-        where T : class
+    public class Live<T>
+        where T : RealmObject, IHasGuidPrimaryKey
     {
-        private readonly Func<Realm, T> query;
+        /// <summary>
+        /// The original live data used to create this instance.
+        /// </summary>
+        private readonly T data;
+
+        private readonly Guid id;
 
         private readonly IRealmFactory realm;
 
         /// <summary>
-        /// The currently retrieved instance of the realm data, sourced from the <see cref="retrievedContext"/>.
-        /// </summary>
-        private T retrievedValue;
-
-        private Realm? retrievedContext;
-
-        /// <summary>
         /// Construct a new instance of live realm data.
         /// </summary>
-        /// <param name="query">A function which initialises and returns the value.</param>
+        /// <param name="data">The realm data.</param>
         /// <param name="realm">A context factory to allow transfer and re-retrieval over thread contexts.</param>
-        public Live(Func<Realm, T> query, IRealmFactory realm)
+        public Live(T data, IRealmFactory realm)
         {
-            this.query = query;
+            this.data = data;
             this.realm = realm;
 
-            retrievedValue = fetchThreadLocalValue();
+            id = data.ID;
         }
 
         /// <summary>
-        /// Access the underlying value directly from realm.
+        /// Perform a write operation on this live object.
         /// </summary>
-        public T Value => fetchThreadLocalValue();
+        /// <param name="perform">The action to perform.</param>
+        public TReturn PerformRead<TReturn>(Func<T, TReturn> perform)
+        {
+            if (data.IsValid && !data.Realm.IsClosed)
+                return perform(data);
+
+            using (var usage = realm.GetForRead())
+                return perform(usage.Realm.Find<T>(id));
+        }
 
         /// <summary>
         /// Perform a write operation on this live object.
@@ -52,38 +56,12 @@ namespace osu.Game.Database
         /// <param name="perform">The action to perform.</param>
         public void PerformUpdate(Action<T> perform)
         {
-            Debug.Assert(ThreadSafety.IsUpdateThread);
-
             using (var usage = realm.GetForWrite())
             {
-                // we are on the update thread so the context should always match our retrieved context.
-                // if this isn't the case things are going to fall over when making changes.
-                Debug.Assert(ReferenceEquals(usage.Realm, retrievedContext));
-
-                perform(retrievedValue);
+                perform(usage.Realm.Find<T>(id));
 
                 usage.Commit();
             }
         }
-
-        /// <summary>
-        /// Fetches the value represented by this instance on the current update thread context.
-        /// </summary>
-        /// <returns></returns>
-        private T fetchThreadLocalValue()
-        {
-            Debug.Assert(ThreadSafety.IsUpdateThread);
-
-            var localThreadContext = realm.Context;
-
-            // if the retrieved context is still valid, no re-retrieval is required.
-            if (retrievedContext?.IsClosed == false && retrievedContext.IsSameInstance(localThreadContext))
-                return retrievedValue;
-
-            retrievedContext = localThreadContext;
-            return retrievedValue = query(retrievedContext);
-        }
-
-        void IRealmBindableActions.RunSetupAction() => fetchThreadLocalValue();
     }
 }
